2a6a24f59e4e28ef7b424978264740263036eec6
Joy Baruah <joy.baruah@wireonelabs.com>
2023-07-16 21:23:06 +0530

    Updated: Exp infp

--- data/exp.js
+++ data/exp.js
@@ -1,18 +1,20 @@
 export const exp = [
-    {
-        "id": 1,
-        "title": "Frontend Developer Intern",
-        "company": "Wireone Labs",
-        "timeline": "Aug - Sep 2022",
-        "description": "Adapted from Reactjs to Native. Identified Bugs and Delivered solutions for the same. Developed and Shipped Production code with new Features.",
-        "skills": ["ReactJs", "React-Native", "Tailwind-CSS", "JavaScript"]
-    },
-    {
-        "id": 2,
-        "title": "Full Stack Developer",
-        "company": "Wireone Labs",
-        "timeline": "Sep 2022 - Present",
-        "description": "Developed, maintained and Deployed Production code in Cloud Services. Written quality Farm Automation code and flows in Node-Red. Created Notification Service using NodeJS & FCM operated by Django.",
-        "skills": ["Python", "Django", "Nodejs", "Firebase", "AWS", "Node-Red"]
-    }
-]
+  {
+    id: 1,
+    title: "Frontend Developer Intern",
+    company: "Wireone Labs",
+    timeline: "Aug - Sep 2022",
+    description:
+      "Adapted from Reactjs to Native. Identified Bugs and Delivered solutions for the same. Developed and Shipped Production code with quality Features.",
+    skills: ["ReactJs", "React-Native", "Tailwind-CSS", "JavaScript"],
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer",
+    company: "Wireone Labs",
+    timeline: "Sep 2022 - Present",
+    description:
+      "Developed, maintained and Deployed Production code in Cloud Services. Written quality Automation code in certain Languages. Created various Services using certain Frameworks.",
+    skills: ["Python", "Django", "Nodejs", "Firebase", "AWS", "Node-Red"],
+  },
+];
