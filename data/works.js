e7f3751212328b5f287057389e71752a2a5005c7
Joy Baruah <joy.baruah@wireonelabs.com>
2023-08-27 20:28:07 +0530

    UPD: works

--- data/works.js
+++ data/works.js
@@ -1,23 +1,31 @@
 export const works = [
-    {
-        "id": 1,
-        "title": "Creative Minds",
-        "link": "https://creative-minds-joybaruah.vercel.app/",
-        "description": "Simple social media site with google authentication and Post and comment features.",
-        "tech": ["NextJs", "Firebase", "Tailwind CSS"]
-    },
-    {
-        "id": 2,
-        "title": "Algorizon Technologies",
-        "link": "https://algorizon.netlify.app/",
-        "description": "A Website for Software company.",
-        "tech": ["ReactJs", "React Router", "Tailwind CSS", "Framer Motion"]
-    },
-    {
-        "id": 2,
-        "title": "Pokedex CLI",
-        "link": "https://github.com/Joybaruah/poke-cli",
-        "description": "A Command Line PokeDex Game.",
-        "tech": ["Golang", "Poke API"]
-    }
-]
+  {
+    id: 1,
+    title: "Creative Minds",
+    link: "https://creative-minds-joybaruah.vercel.app/",
+    description:
+      "Simple social media site with google authentication and Post and comment features.",
+    tech: ["NextJs", "Firebase", "Tailwind CSS"],
+  },
+  {
+    id: 2,
+    title: "Algorizon Technologies",
+    link: "https://algorizon.netlify.app/",
+    description: "A Website for Software company.",
+    tech: ["ReactJs", "React Router", "Tailwind CSS", "Framer Motion"],
+  },
+  {
+    id: 3,
+    title: "Pokedex CLI",
+    link: "https://github.com/Joybaruah/poke-cli",
+    description: "A Command Line PokeDex Game.",
+    tech: ["Golang", "Poke API"],
+  },
+  {
+    id: 4,
+    title: "read-tech",
+    link: "https://read-tech.vercel.app/",
+    description: "Blog site with tech related Articles.",
+    tech: ["NextJs", "Tailwind CSS", "MDX"],
+  },
+];
